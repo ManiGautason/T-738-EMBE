ab8a91b2c73e7411c18013bbb88fc702b2f6f289
magnusbenedikt <83277545+magnusbenedikt@users.noreply.github.com>
2023-09-05 20:20:09 +0000

    Update digital_in.h
    fixed indentation for readability

--- Projects/Project_1/include/digital_in.h
+++ Projects/Project_1/include/digital_in.h
@@ -2,12 +2,15 @@
 #include <avr/interrupt.h>
 
 class Digital_in {
-public:
-  Digital_in(uint8_t pin);
-  void init();
-  bool is_hi();
-  bool is_lo();
-  void initINT(); 
-private:
-  uint8_t pinMask;
+
+  public:
+    Digital_in(uint8_t pin);
+    void init();
+    bool is_hi();
+    bool is_lo();
+    void initINT(); 
+  
+  private:
+    uint8_t pinMask;
+
 };
