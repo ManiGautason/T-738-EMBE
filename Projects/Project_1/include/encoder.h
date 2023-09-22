4e1518313c977f0a1aeb6dc6b7c05fb4f1348f2e
magnusbenedikt <83277545+magnusbenedikt@users.noreply.github.com>
2023-09-22 11:02:50 +0000

    part1imp
    Implemented part 1

--- Projects/Project_1/include/encoder.h
+++ Projects/Project_1/include/encoder.h
@@ -1,19 +1,42 @@
+// #include <stdint.h>
+
+// class Encoder {
+
+//     public:
+//         Encoder(){
+//             counter = 0;
+//             lastA = 0;
+//             lastB = 0;
+//         };
+//         int position();
+//         volatile int counter;
+//         void updateCount(bool A, bool B);
+
+//     private:
+//         bool lastA;
+//         bool lastB;
+        
+// };
+
+
+#ifndef ENCODER_H
+#define ENCODER_H
+
 #include <stdint.h>
 
 class Encoder {
+public:
+    Encoder();
+    int position();
+    void updateCount(bool A, bool B);
+    float speedPPS();
+    float speedRPM();
 
-    public:
-        Encoder(){
-            counter = 0;
-            lastA = 0;
-            lastB = 0;
-        };
-        int position();
-        volatile int counter;
-        void updateCount(bool A, bool B);
-
-    private:
-        bool lastA;
-        bool lastB;
-        
+private:
+    bool lastA;
+    bool lastB;
+    volatile int counter;
+    unsigned long prevTime;
 };
+
+#endif
--- Projects/Project_1/src/encoder.cpp
+++ Projects/Project_1/src/encoder.cpp
@@ -1,19 +1,58 @@
-#include <encoder.h>
+// #include <encoder.h>
 
-Encoder encoder;
+// Encoder encoder;
+
+// int Encoder::position() {
+//     return encoder.counter;
+// }
+
+// void Encoder::updateCount(bool A, bool B) {
+//     //if(A != encoder.lastA) {
+//         if(B != A) {
+//             encoder.counter++;
+//         }
+//         else {
+//             encoder.counter--;
+//         }
+//     //}
+//     //encoder.lastA = A;
+// }
+
+#include "Encoder.h"
+#include <Arduino.h> // Include this if you're using Arduino IDE and want to use millis()
+
+Encoder::Encoder() {
+    counter = 0;
+    lastA = 0;
+    lastB = 0;
+    prevTime = 0;
+}
 
 int Encoder::position() {
-    return encoder.counter;
+    return counter;
 }
 
 void Encoder::updateCount(bool A, bool B) {
-    //if(A != encoder.lastA) {
-        if(B != A) {
-            encoder.counter++;
-        }
-        else {
-            encoder.counter--;
-        }
-    //}
-    //encoder.lastA = A;
+    if (B != A) {
+        counter++;
+    } else {
+        counter--;
+    }
 }
+
+float Encoder::speedPPS() {
+    unsigned long currentTime = millis();
+    unsigned long deltaTime = currentTime - prevTime;
+    float pps = static_cast<float>(counter) / deltaTime * 1000.0;
+    prevTime = currentTime;
+    return pps;
+}
+
+float Encoder::speedRPM() {
+    float pps = speedPPS();
+    float rpm = pps / (encoder_pulses_per_revolution / 2) * 60.0;
+    return rpm;
+}
+
+// Define the number of encoder pulses per revolution (you need to replace this value)
+const int encoder_pulses_per_revolution = 1000; // Replace with your encoder's value
