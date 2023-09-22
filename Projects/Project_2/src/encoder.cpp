8916a4e6af9090d23d0bd06fc2c28df9c1c1318d
Máni Gautason <gautason.mani@gmail.com>
2023-09-22 14:08:37 +0000

    Part1Working

--- Projects/Project_2/src/encoder.cpp
+++ Projects/Project_2/src/encoder.cpp
@@ -1,59 +1,19 @@
-// #include <encoder.h>
+#include <encoder.h>
 
-// Encoder encoder;
-
-// int Encoder::position() {
-//     return encoder.counter;
-// }
-
-// void Encoder::updateCount(bool A, bool B) {
-//     //if(A != encoder.lastA) {
-//         if(B != A) {
-//             encoder.counter++;
-//         }
-//         else {
-//             encoder.counter--;
-//         }
-//     //}
-//     //encoder.lastA = A;
-// }
-
-
-#include "Encoder.h"
-#include <Arduino.h> // Include this if you're using Arduino IDE and want to use millis()
-
-Encoder::Encoder() {
-    counter = 0;
-    lastA = 0;
-    lastB = 0;
-    prevTime = 0;
-}
+Encoder encoder;
 
 int Encoder::position() {
-    return counter;
+    return encoder.counter;
 }
 
 void Encoder::updateCount(bool A, bool B) {
-    if (B != A) {
-        counter++;
-    } else {
-        counter--;
-    }
+    //if(A != encoder.lastA) {
+        if(B != A) {
+            encoder.counter++;
+        }
+        else {
+            encoder.counter--;
+        }
+    //}
+    //encoder.lastA = A;
 }
-
-float Encoder::speedPPS() {
-    unsigned long currentTime = millis();
-    unsigned long deltaTime = currentTime - prevTime;
-    float pps = static_cast<float>(counter) / deltaTime * 1000.0;
-    prevTime = currentTime;
-    return pps;
-}
-
-float Encoder::speedRPM() {
-    float pps = speedPPS();
-    float rpm = pps / (encoder_pulses_per_revolution / 2) * 60.0;
-    return rpm;
-}
-
-// Define the number of encoder pulses per revolution (you need to replace this value)
-const int encoder_pulses_per_revolution = 1000; // Replace with your encoder's value
--- Projects/Project_2/src/main.cpp
+++ Projects/Project_2/src/main.cpp
@@ -8,31 +8,54 @@ Encoder enc;
 Digital_in A(2);//PD2 for the signal A, D2
 Digital_in B(3);//PD3 for the signal B, D3
 
-int main(){
+Digital_out LED(5);// Port B
 
-  Digital_out LED(5);// Port B
-  A.initINT();    // Initialize A signal as interrupt
-  B.init(); // Initialize B signal
-  LED.init();
-  // Enable global interrupts
-  sei();
-  Serial.begin(115200);
-  int count = 0;
-  int countB = 0;
-  while (1)
-   {
-    //enc.updateCount(A.is_hi(),B.is_hi());
+int count = 0;
+int countA = 0;
+int countB = 0;
+
+unsigned long StateChangeTime = 0;
+unsigned long PreviousStateChangeTime = 0;
+unsigned long StatePeriod = 0;
+unsigned long previousMillis = 0;
+const unsigned long interval = 10; // 1 second (in milliseconds)
+void setup(){
+    Serial.begin(115200);
+    A.initINT();    // Initialize A signal as interrupt
+    B.init(); // Initialize B signal
+    LED.init();
+    // Enable global interrupts
+    sei();
+}
+void loop(){
+
+  unsigned long currentMillis = millis();
+  count = enc.position();
+  // Serial.println(count);
+  while(previousMillis - currentMillis <= interval) {
+    // It's been 1 second, perform your action here
+    previousMillis = millis();
     countB = enc.position();
-    //Serial.println(countB);
-    if(count != countB) {
-      count = countB;
-      LED.toggle();
-      
-      Serial.println(countB);
-    }
-    //countB = enc.position();
-    //Serial.println(countB);
-  } 
+    // Update the previousMillis to the current time for the next interval
+    //previousMillis = currentMillis;
+  }
+  int PulsesPerSecond = countB - count;
+  float RPM = ((float)PulsesPerSecond * 100 / (14.0 * 100.0)) * 60.0;
+  int RPMint = int(RPM);
+  //float RPM = (PulsesPerSecond / (7) * 60.0);
+  Serial.println(RPMint);
+
+  //RPM = (pulsesPerSecond * 60.0) / pulsesPerRevolution;
+
+    // countB = enc.position();
+    // if(count != countB) {
+    //   count = countB;
+    //   StateChangeTime = millis();
+    //   StatePeriod = StateChangeTime - PreviousStateChangeTime;
+    //   PreviousStateChangeTime = StateChangeTime;
+    //   Serial.print('RPM: ');
+    //   Serial.println(60000/(StatePeriod * 14 * 100* 2));
+    // }
 }
   
 // Define the ISR for INT0 (external interrupt)
