20851eb2ebfd20e0ba94a7c27fcd491a39b4215d
magnusbenedikt <83277545+magnusbenedikt@users.noreply.github.com>
2023-10-04 11:24:57 +0000

    changes
    yessir

--- Projects/Project_3/src/OpState.cpp
+++ Projects/Project_3/src/OpState.cpp
@@ -1,22 +1,23 @@
 #include "InitState.h"
 #include "Context.h"
 #include "Arduino.h"
+#include "OpState.h"
 
-void GreenState::on_do() {
+void OpState::on_do() {
 }
 
-void GreenState::on_entry() {
+void OpState::on_entry() {
     Serial.println("Greenstate entry: turn ON GREEN LIGHT");
 }
 
-void GreenState::on_exit() {
+void OpState::on_exit() {
     Serial.println("Greenstate exit: turn OFF GREEN LIGHT");
 }
 
-void GreenState::on_event1() {
+void OpState::on_event1() {
 }
 
-void GreenState::on_event2() {
+void OpState::on_event2() {
     // optionally do something on transition
-    this->context_->transition_to(new YellowState);
+    this->context_->transition_to(new InitState);
 }
