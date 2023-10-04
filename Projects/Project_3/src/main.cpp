3dbca61ed960dc704ded2c6841219e6928872077
Máni Gautason <gautason.mani@gmail.com>
2023-10-04 11:13:22 +0000

    Update main.cpp

--- Projects/Project_3/src/main.cpp
+++ Projects/Project_3/src/main.cpp
@@ -5,27 +5,13 @@
 #include <Arduino.h>
 #include <avr/interrupt.h>
 #include <analog_out.h>
-//#include <P_controller.h>
+
 Analog_out analog(4);
-//P_controller Pcon(7,120,255);
 Encoder enc;
 Digital_in A(2);//PD2 for the signal A, D2
 Digital_in B(3);//PD3 for the signal B, D3
 
-//Digital_out LED(5);// Port B
 
-int count = 0;
-int countA = 0;
-int countB = 0;
-int PV = 0;
-float PWM = 0;
-unsigned long StateChangeTime = 0;
-unsigned long PreviousStateChangeTime = 0;
-unsigned long StatePeriod = 0;
-unsigned long previousTime = 0;
-unsigned long previousTimeStep = 0;
-const unsigned long interval = 10; // 1 second (in milliseconds)
-int setpoint = 0;
 
 void setup(){
     Serial.begin(115200);
