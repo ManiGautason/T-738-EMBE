b93dae50a9c9a9bb00a8bcc9c92f43b2b0e18a3a
magnusbenedikt <83277545+magnusbenedikt@users.noreply.github.com>
2023-10-06 13:26:38 +0000

    P and PI controller classes fixes and test
    Merged the P_controller and controller classes, and implemented some test to test them

---
+++ Projects/Project_3/lib/PI_controller/include/PI_controller.h
@@ -0,0 +1,17 @@
+#ifndef PI_CONTROLLER
+#define PI_CONTROLLER
+
+#include "controller.h"
+
+class PI_controller : public controller {
+public:
+    PI_controller(double K_p, double K_i, int max_rpm, int max_pwm, float dt);
+    int update(double Setpoint, double current_Val) override;
+
+private:
+    double k_i;
+    double Error;
+    double dt_;
+};
+
+#endif
