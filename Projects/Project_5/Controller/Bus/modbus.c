ffbcc4ea8cae9f072bd4bca0e94a5ee5bc043b41
magnusbenedikt <83277545+magnusbenedikt@users.noreply.github.com>
2023-11-08 11:04:18 +0000

    crc fix fix
    crc fixed for the temp arduino

--- Projects/Project_5/Controller/Bus/modbus.c
+++ Projects/Project_5/Controller/Bus/modbus.c
@@ -1,138 +1,3 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <termios.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-
-#define WRITE_SINGLE_REGISTER 6
-#define READ_HOLDING_REGISTERS 3
-int file, count;
-
-
-
-void createModbusMessage(uint8_t* buffer, uint8_t serverAddress, uint8_t cmd, uint16_t serverRegister, uint16_t value, uint16_t CRC) {
-    buffer[0] = serverAddress;            // Server address (cast to byte)
-    buffer[1] = cmd; // Function code (for writing a single register)
-
-    // Server register and value (both are 16-bit values, big-endian format)
-    buffer[2] = (uint8_t)((serverRegister >> 8) & 0xFF); // High byte of serverRegister
-    buffer[3] = (uint8_t)(serverRegister & 0xFF);       // Low byte of serverRegister
-
-    buffer[4] = (uint8_t)((value >> 8) & 0xFF); // High byte of value
-    buffer[5] = (uint8_t)(value & 0xFF);       // Low byte of value
-
-
-    // CRC (16-bit value, little-endian format)
-    buffer[6] = (uint8_t)((CRC >> 8) & 0xFF); // High byte of CRC(CRC & 0xFF);       // Low byte of CRC
-    buffer[7] = (uint8_t)(CRC & 0xFF);       // Low byte of CRC
-}
-
-
-void parseModbusResponse(unsigned char* response, int length) {
-    printf("Slave Address: %02X\n", response[0]);
-    printf("Function Code: %02X\n", response[1]);
-    printf("Byte Count: %02X\n", response[2]);
-
-    printf("Data: ");
-    for (int i = 3; i < length - 2; i++) {
-        printf("%02X ", response[i]);
-    }
-    printf("\n");
-
-    printf("CRC: %02X %02X\n", response[length - 2], response[length - 1]);
-}
-
-int main(int argc, char *argv[]) {
-    int CRC = 43737;
-    uint8_t modbusSendBuffer[8];
-    uint8_t modbusRecieveBuffer[8];
-    
-    if (argc != 5) {
-        printf("Invalid number of arguments, exiting!\n");
-        return -2;
-    }
-
-    if ((file = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY)) < 0) {
-        perror("UART: Failed to open the file.\n");
-        return -1;
-    }
-
-    struct termios options;
-
-    tcgetattr(file, &options);
-
-    options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
-    options.c_iflag = IGNPAR | ICRNL;
-    tcflush(file, TCIFLUSH);
-    tcsetattr(file, TCSANOW, &options);
-
-    if (argc >= 4) {
-        int ServerAddress = atoi(argv[1]);
-        int Command = atoi(argv[2]);
-        int RegisterAddress = atoi(argv[3]);
-        int Value = atoi(argv[4]);
-
-        if (Command == WRITE_SINGLE_REGISTER) {
-            createModbusMessage(modbusSendBuffer, ServerAddress, WRITE_SINGLE_REGISTER, RegisterAddress, Value, CRC);
-            printf("Modbus Message: ");
-            for (int i = 0; i < 8; i++) {
-                printf("%02X ", modbusSendBuffer[i]);
-            }
-
-
-            if ((count = write(file, &modbusSendBuffer, 8))<0){
-                perror("Failed to write to the output\n");
-                return -1;
-            }
-            printf("Message transmitted\n");
-            usleep(100000);
-
-            printf("Reading\n");
-            if ((count = read(file, (void*)modbusRecieveBuffer, 8))<0){
-                perror("Failed to read from the input\n");
-                return -1;
-            }
-
-            parseModbusResponse(modbusRecieveBuffer, count);
-
-            //send(ServerAddress, RegisterAddress, Value, CRC);
-        } else if (Command == READ_HOLDING_REGISTERS) {
-            createModbusMessage(modbusSendBuffer, ServerAddress, READ_HOLDING_REGISTERS, RegisterAddress, Value, CRC);
-            printf("Modbus Message: ");
-            for (int i = 0; i < 8; i++) {
-                printf("%02X ", modbusSendBuffer[i]);
-
-            }
-            if ((count = write(file, &modbusSendBuffer, 8))<0){
-                perror("Failed to write to the output\n");
-                return -1;
-            }
-            printf("Message transmitted\n");
-            usleep(10000);
-
-            printf("Reading\n");
-            if ((count = read(file, (void*)modbusSendBuffer, 8)) < 0){
-                perror("Failed to read from the input\n");
-                return -1;
-            }
-            for (int i = 0; i < 8; i++) {
-                printf("%02X ", modbusSendBuffer[i]);
-
-            }
-            //parseModbusResponse(modbusRecieveBuffer, count);
-        }
-    }
-
-    close(file);
-    return 0;
-}
-
-
-
-
-
 // #include <stdio.h>
 // #include <fcntl.h>
 // #include <unistd.h>
@@ -145,24 +10,9 @@ int main(int argc, char *argv[]) {
 // #define READ_HOLDING_REGISTERS 3
 // int file, count;
 
-// // Compute the MODBUS RTU CRC
-// uint16_t ModRTU_CRC(uint8_t buf[], int len) {
-//     uint16_t crc = 0xFFFF;
-//     for (int pos = 0; pos < len; pos++) {
-//         crc ^= (uint16_t)buf[pos]; // XOR byte into least sig. byte of crc
-//         for (int i = 8; i != 0; i--) { // Loop over each bit
-//             if ((crc & 0x0001) != 0) { // If the LSB is set
-//                 crc >>= 1; // Shift right and XOR 0xA001
-//                 crc ^= 0xA001;
-//             }
-//             else // Else LSB is not set
-//                 crc >>= 1; // Just shift right
-//         }
-//     }
-//     return crc;
-// }
 
-// void createModbusMessage(uint8_t* buffer, uint8_t serverAddress, uint8_t cmd, uint16_t serverRegister, uint16_t value) {
+
+// void createModbusMessage(uint8_t* buffer, uint8_t serverAddress, uint8_t cmd, uint16_t serverRegister, uint16_t value, uint16_t CRC) {
 //     buffer[0] = serverAddress;            // Server address (cast to byte)
 //     buffer[1] = cmd; // Function code (for writing a single register)
 
@@ -175,7 +25,6 @@ int main(int argc, char *argv[]) {
 
 
 //     // CRC (16-bit value, little-endian format)
-//     uint16_t CRC = ModRTU_CRC(buffer,6);
 //     buffer[6] = (uint8_t)((CRC >> 8) & 0xFF); // High byte of CRC(CRC & 0xFF);       // Low byte of CRC
 //     buffer[7] = (uint8_t)(CRC & 0xFF);       // Low byte of CRC
 // }
@@ -195,13 +44,11 @@ int main(int argc, char *argv[]) {
 //     printf("CRC: %02X %02X\n", response[length - 2], response[length - 1]);
 // }
 
-
-
 // int main(int argc, char *argv[]) {
-
+//     int CRC = 43737;
 //     uint8_t modbusSendBuffer[8];
 //     uint8_t modbusRecieveBuffer[8];
-
+    
 //     if (argc != 5) {
 //         printf("Invalid number of arguments, exiting!\n");
 //         return -2;
@@ -215,24 +62,20 @@ int main(int argc, char *argv[]) {
 //     struct termios options;
 
 //     tcgetattr(file, &options);
-//     cfmakeraw(&options);            // set as raw
-//     options.c_lflag &= ~ICANON;
-//     options.c_cc[VTIME] = 0;  
-//     options.c_cc[VMIN] = 8;
+
 //     options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
 //     options.c_iflag = IGNPAR | ICRNL;
 //     tcflush(file, TCIFLUSH);
 //     tcsetattr(file, TCSANOW, &options);
 
-
-//     if (argc >= 4) {
+//     if (argc >= 4)
 //         int ServerAddress = atoi(argv[1]);
 //         int Command = atoi(argv[2]);
 //         int RegisterAddress = atoi(argv[3]);
 //         int Value = atoi(argv[4]);
- 
+
 //         if (Command == WRITE_SINGLE_REGISTER) {
-//             createModbusMessage(modbusSendBuffer, ServerAddress, WRITE_SINGLE_REGISTER, RegisterAddress, Value);
+//             createModbusMessage(modbusSendBuffer, ServerAddress, WRITE_SINGLE_REGISTER, RegisterAddress, Value, CRC);
 //             printf("Modbus Message: ");
 //             for (int i = 0; i < 8; i++) {
 //                 printf("%02X ", modbusSendBuffer[i]);
@@ -244,28 +87,19 @@ int main(int argc, char *argv[]) {
 //                 return -1;
 //             }
 //             printf("Message transmitted\n");
-//             usleep(10000);
+//             usleep(100000);
 
 //             printf("Reading\n");
-//             if ((count = read(file, (void*)modbusRecieveBuffer, 7))<0){
+//             if ((count = read(file, (void*)modbusRecieveBuffer, 8))<0){
 //                 perror("Failed to read from the input\n");
 //                 return -1;
 //             }
 
 //             parseModbusResponse(modbusRecieveBuffer, count);
-//             printf("Count%d", count);
-//             uint16_t recievedCRC = ((uint16_t)modbusRecieveBuffer[6] << 8) | modbusRecieveBuffer[7];
-//             printf("ACRC: %02X\n",modbusRecieveBuffer[6]);
-//             printf("BCRC: %02X\n",modbusRecieveBuffer[7]);
-//             printf("Checked: %04X\n", ModRTU_CRC(modbusRecieveBuffer,5));
-//             if(recievedCRC == ModRTU_CRC(modbusRecieveBuffer,5)){
-//                 printf("CRC OK\n");
-//             } else {
-//                 printf("CRC Error\n");
-//             }
 
+//             //send(ServerAddress, RegisterAddress, Value, CRC);
 //         } else if (Command == READ_HOLDING_REGISTERS) {
-//             createModbusMessage(modbusSendBuffer, ServerAddress, READ_HOLDING_REGISTERS, RegisterAddress, Value);
+//             createModbusMessage(modbusSendBuffer, ServerAddress, READ_HOLDING_REGISTERS, RegisterAddress, Value, CRC);
 //             printf("Modbus Message: ");
 //             for (int i = 0; i < 8; i++) {
 //                 printf("%02X ", modbusSendBuffer[i]);
@@ -279,24 +113,193 @@ int main(int argc, char *argv[]) {
 //             usleep(10000);
 
 //             printf("Reading\n");
-//             if ((count = read(file, (void*)modbusRecieveBuffer, 8)) < 0){
+//             if ((count = read(file, (void*)modbusSendBuffer, 8)) < 0){
 //                 perror("Failed to read from the input\n");
 //                 return -1;
 //             }
+//             for (int i = 0; i < 8; i++) {
+//                 printf("%02X ", modbusSendBuffer[i]);
 
-//             parseModbusResponse(modbusRecieveBuffer, count);
-//             uint16_t recievedCRC = ((uint16_t)modbusRecieveBuffer[6] << 8) | modbusRecieveBuffer[7];
-//             printf("CRC: %04X\n", recievedCRC);
-//             printf("Checked: %04X\n", ModRTU_CRC(modbusRecieveBuffer,5));
-//             if(recievedCRC == ModRTU_CRC(modbusRecieveBuffer,5)){
-//                 printf("CRC OK\n");
-//             } else {
-//                 printf("CRC Error\n");
 //             }
-
+//             //parseModbusResponse(modbusRecieveBuffer, count);
 //         }
 //     }
 
 //     close(file);
 //     return 0;
 // }
+
+
+
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define WRITE_SINGLE_REGISTER 6
+#define READ_HOLDING_REGISTERS 3
+int file, count;
+
+// Compute the MODBUS RTU CRC
+uint16_t ModRTU_CRC(uint8_t buf[], int len) {
+    uint16_t crc = 0xFFFF;
+    for (int pos = 0; pos < len; pos++) {
+        crc ^= (uint16_t)buf[pos]; // XOR byte into least sig. byte of crc
+        for (int i = 8; i != 0; i--) { // Loop over each bit
+            if ((crc & 0x0001) != 0) { // If the LSB is set
+                crc >>= 1; // Shift right and XOR 0xA001
+                crc ^= 0xA001;
+            }
+            else // Else LSB is not set
+                crc >>= 1; // Just shift right
+        }
+    }
+    return crc;
+}
+
+void createModbusMessage(uint8_t* buffer, uint8_t serverAddress, uint8_t cmd, uint16_t serverRegister, uint16_t value) {
+    buffer[0] = serverAddress;            // Server address (cast to byte)
+    buffer[1] = cmd; // Function code (for writing a single register)
+
+    // Server register and value (both are 16-bit values, big-endian format)
+    buffer[2] = (uint8_t)((serverRegister >> 8) & 0xFF); // High byte of serverRegister
+    buffer[3] = (uint8_t)(serverRegister & 0xFF);       // Low byte of serverRegister
+
+    buffer[4] = (uint8_t)((value >> 8) & 0xFF); // High byte of value
+    buffer[5] = (uint8_t)(value & 0xFF);       // Low byte of value
+
+
+    // CRC (16-bit value, little-endian format)
+    uint16_t CRC = ModRTU_CRC(buffer,6);
+    buffer[6] = (uint8_t)((CRC >> 8) & 0xFF); // High byte of CRC(CRC & 0xFF);       // Low byte of CRC
+    buffer[7] = (uint8_t)(CRC & 0xFF);       // Low byte of CRC
+}
+
+
+void parseModbusResponse(unsigned char* response, int length) {
+    printf("Slave Address: %02X\n", response[0]);
+    printf("Function Code: %02X\n", response[1]);
+    printf("Byte Count: %02X\n", response[2]);
+
+    printf("Data: ");
+    for (int i = 3; i < length - 2; i++) {
+        printf("%02X ", response[i]);
+    }
+    printf("\n");
+
+    printf("CRC: %02X %02X\n", response[length - 1], response[length - 2]);
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+    uint8_t modbusSendBuffer[8];
+    uint8_t modbusRecieveBuffer[8];
+
+    if (argc != 5) {
+        printf("Invalid number of arguments, exiting!\n");
+        return -2;
+    }
+
+    if ((file = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY)) < 0) {
+        perror("UART: Failed to open the file.\n");
+        return -1;
+    }
+
+    struct termios options;
+
+    tcgetattr(file, &options);
+    cfmakeraw(&options);            // set as raw
+    options.c_lflag &= ~ICANON;
+    options.c_cc[VTIME] = 0;  
+    options.c_cc[VMIN] = 8;
+    options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
+    options.c_iflag = IGNPAR | ICRNL;
+    tcflush(file, TCIFLUSH);
+    tcsetattr(file, TCSANOW, &options);
+
+
+    if (argc >= 4) {
+        int ServerAddress = atoi(argv[1]);
+        int Command = atoi(argv[2]);
+        int RegisterAddress = atoi(argv[3]);
+        int Value = atoi(argv[4]);
+ 
+        if (Command == WRITE_SINGLE_REGISTER) {
+            createModbusMessage(modbusSendBuffer, ServerAddress, WRITE_SINGLE_REGISTER, RegisterAddress, Value);
+            printf("Modbus Message: ");
+            for (int i = 0; i < 8; i++) {
+                printf("%02X ", modbusSendBuffer[i]);
+            }
+
+
+            if ((count = write(file, &modbusSendBuffer, 8))<0){
+                perror("Failed to write to the output\n");
+                return -1;
+            }
+            printf("Message transmitted\n");
+            usleep(10000);
+
+            printf("Reading\n");
+            if ((count = read(file, (void*)modbusRecieveBuffer, 7))<0){
+                perror("Failed to read from the input\n");
+                return -1;
+            }
+
+            parseModbusResponse(modbusRecieveBuffer, count);
+            printf("Count%d", count);
+            uint16_t recievedCRC = ((uint16_t)modbusRecieveBuffer[6] << 8) | modbusRecieveBuffer[7];
+            printf("ACRC: %02X\n",modbusRecieveBuffer[6]);
+            printf("BCRC: %02X\n",modbusRecieveBuffer[7]);
+            printf("Checked: %04X\n", ModRTU_CRC(modbusRecieveBuffer,5));
+            if(recievedCRC == ModRTU_CRC(modbusRecieveBuffer,5)){
+                printf("CRC OK\n");
+            } else {
+                printf("CRC Error\n");
+            }
+
+        } else if (Command == READ_HOLDING_REGISTERS) {
+            createModbusMessage(modbusSendBuffer, ServerAddress, READ_HOLDING_REGISTERS, RegisterAddress, Value);
+            printf("Modbus Message: ");
+            for (int i = 0; i < 8; i++) {
+                printf("%02X ", modbusSendBuffer[i]);
+
+            }
+            if ((count = write(file, &modbusSendBuffer, 8))<0){
+                perror("Failed to write to the output\n");
+                return -1;
+            }
+            printf("Message transmitted\n");
+            usleep(10000);
+
+            printf("Reading\n");
+            if ((count = read(file, (void*)modbusRecieveBuffer, 7)) < 0){
+                perror("Failed to read from the input\n");
+                return -1;
+            }
+
+            parseModbusResponse(modbusRecieveBuffer, count);
+            for (int i = 0; i < 7; i++) {
+                printf("%02X ", modbusRecieveBuffer[i]);
+            }
+            uint16_t recievedCRC = ((uint16_t)modbusRecieveBuffer[6] << 8) | modbusRecieveBuffer[5];
+            printf("CRC: %04X\n", recievedCRC);
+            printf("Checked: %04X\n", ModRTU_CRC(modbusRecieveBuffer,5));
+            if(recievedCRC == ModRTU_CRC(modbusRecieveBuffer,5)){
+                printf("CRC OK\n");
+            } else {
+                printf("CRC Error\n");
+            }
+
+        }
+    }
+
+    close(file);
+    return 0;
+}
